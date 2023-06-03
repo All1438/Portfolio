827f29c9b3c4ce8a2ccaa940aeb8daba7298eaeb
Ali Nando <suprenando12@gmail.com>
2023-06-03 20:27:06 +0200

    3eme deploy Portfolio

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ import {BiDownload} from 'react-icons/bi'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} className="btn" target='_blank ' rel='noreferrer' ><span>Download CV</span><BiDownload className='cta__icon'/ > </a>
+        <a href={CV} className="btn" target='_blank' rel='noreferrer' ><span>Download CV</span><BiDownload className='cta__icon'/ > </a>
         <a href="#contact" className='btn btn-primary'>Let's talk</a>
     </div>
   )
